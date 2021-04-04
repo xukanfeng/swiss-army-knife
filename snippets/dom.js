2208a5edde944b9ae8e869a3f5b559b52a519dd6
徐侃锋 <xu_kanfeng@192.168.0.105>
2021-04-04 22:49:39 +0800

    update snippets

--- snippets/dom.js
+++ snippets/dom.js
@@ -8,3 +8,46 @@ function commonParentNode(oNode1, oNode2) {
         return commonParentNode(oNode1.parentNode, oNode2)
     }
 }
+
+/**
+ * 返回 dom tree 的高度
+ */
+function getHeight(tree) {
+  if (!tree) return 0
+
+  let height = 0
+  for (let child of tree.children) {
+    height = Math.max(height, getHeight(child))
+  }
+  return height + 1
+}
+
+/**
+ * 返回 dom tree 包含的元素标签名
+ */
+function getTags(tree) {
+  const set = new Set()
+  const queue = [tree]
+
+  while (queue.length) {
+    const el = queue.shift()
+    set.add(el.tagName.toLowerCase())
+    queue.push(...el.children)
+  }
+  return [...set]
+}
+
+/**
+ * 返回 dom tree 指定元素右边的元素
+ */
+function nextRightSibling(root, target) {
+  const queue = [root]
+  while(queue.length) {
+    const el = queue.shift()
+    if (el === target) {
+      return queue.length ? queue.shift() : null
+    }
+    queue.push(...el.children)
+  }
+  return null
+}
