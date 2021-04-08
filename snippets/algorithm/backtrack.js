b26b496449b3385f0b22f019ce054b90cfd6774a
xukanfeng <xukanfeng1989@hotmail.com>
2021-04-08 11:01:16 +0800

    update snippets

--- snippets/algorithm/backtrack.js
+++ snippets/algorithm/backtrack.js
@@ -1,22 +1,4 @@
 {
-  function subset(arr) {
-    const ans = []
-
-    function backtrack(arr, track, start) {
-      ans.push([...track])
-
-      for (let i = start; i < arr.length; i++) {
-        track.push(arr[i])
-        backtrack(arr, track, i + 1)
-        track.pop()
-      }
-    }
-    backtrack(arr, [], 0)
-
-    return ans
-  }
-  // console.log("subset", subset([1,2,3]))
-
   function combination(arr, k) {
     const ans = []
 
